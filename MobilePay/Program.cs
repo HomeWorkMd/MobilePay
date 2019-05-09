c923089ad62de3476aa4dbade413dbbc1888ef36
Marty <dauciunas@gmail.com>
2019-05-09 11:59:12 +0300

    init

---
+++ MobilePay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobilePay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
